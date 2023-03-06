5efd6d0d10c80c557ba3fad73abb47327d84c50a
varma <jyothendravarma.d@jarustech.com>
2023-03-06 11:38:29 +0530

    form added

--- angular-getstarted/src/app/app.component.ts
+++ angular-getstarted/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titlex = 'getting started';
+  titlex = 'started';
+  myemail:string = "jv@gmail.com";
+  myChoice:boolean  = false;
+
+  OnSubmit = () =>{
+    alert("login done");
+    console.log(`email: ${this.myemail}, choice: ${this.myChoice}`);
+  }
 }
--- angular-getstarted/src/app/app.module.ts
+++ angular-getstarted/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
