c8cc88f006f4384762cffb54d4453e99afb6b9be
varma <jyothendravarma.d@jarustech.com>
2023-03-10 23:57:28 +0530

    adjust icon sizes

--- angular-getstarted/src/app/voter/voter.component.ts
+++ angular-getstarted/src/app/voter/voter.component.ts
@@ -3,13 +3,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-voter',
   template: `
-        <h2>{{name}}</h2>
-        <button class="margin-5" mat-fab (click)="vote(true)" [disabled]="didVote">
-          <mat-icon>thumb_up</mat-icon>
+        <h3>{{name}}</h3>
+        <button class="margin-5" mat-mini-fab (click)="vote(true)" [disabled]="didVote">
+          <mat-icon  class="icon-display">thumb_up</mat-icon>
         </button>
 
-        <button class="margin-5" mat-fab color="warn" (click)="vote(false)" [disabled]="didVote">
-          <mat-icon>thumb_down</mat-icon>
+        <button class="margin-5" mat-mini-fab color="warn" (click)="vote(false)" [disabled]="didVote">
+          <mat-icon  class="icon-display">thumb_down</mat-icon>
         </button>`,
   styleUrls: ['./voter.component.css']
 })
